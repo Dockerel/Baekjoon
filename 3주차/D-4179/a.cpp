78c2e6eac0a0a22b1196b43db0a1f0098c07e2ce
Giheon Do <dgh0001@naver.com>
2024-03-12 22:13:43 +0900

    .

---
+++ 3주차/D-4179/a.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int r, c;
+int maze[1004][1004];
+string s;
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  cin >> r >> c;
+  for (int i = 0; i < r; i++) {
+    cin >> s;
+    for (int j = 0; j < s.length(); j++) {
+    }
+  }
+
+  return 0;
+}
