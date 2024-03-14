621af816ab4a2ae3d8852c201a0ec88fea27d62c
Giheon Do <dgh0001@naver.com>
2024-03-14 21:31:10 +0900

    .

--- 3주차/E-12869/a.cpp
+++ 3주차/E-12869/a.cpp
@@ -1,26 +1,61 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, ret = 0;
+int n, health, ret, x, y, z, nx, ny, nz;
 int dmg[3] = {9, 3, 1};
-int h[3];
+int dx[6] = {9, 9, 3, 3, 1, 1};
+int dy[6] = {3, 1, 9, 1, 3, 9};
+int dz[6] = {1, 3, 1, 9, 9, 1};
+vector<int> h;
 
 void Input() {
   cin >> n;
   for (int i = 0; i < n; i++) {
-    cin >> h[i];
+    cin >> health;
+    h.push_back(health);
   }
+  for (int i = 0; i <= 3 - n; i++) h.push_back(0);
 }
 
-void bfs() {}
+void bfs() {
+  queue<vector<int>> Q;
+  Q.push(h);
+  while (!Q.empty()) {
+    vector<int> tmp = Q.front();
+    Q.pop();
+
+    x = tmp[0];
+    y = tmp[1];
+    z = tmp[2];
+    ret = tmp[3];
+
+    for (int i = 0; i < 6; i++) {
+      nx = x - dx[i];
+      ny = y - dy[i];
+      nz = z - dz[i];
+      if (nx <= 0 && ny <= 0 && nz <= 0) {
+        cout << ret + 1 << "\n";
+        exit(0);
+      }
+      tmp.clear();
+      tmp.push_back(nx);
+      tmp.push_back(ny);
+      tmp.push_back(nz);
+      tmp.push_back(ret + 1);
+      Q.push(tmp);
+    }
+  }
+  return;
+}
 
 void Solution() {
-  // bfs 느낌으로 싹 돌리기
   bfs();
+  return;
 }
 
 void Solve() {
   Input();
   Solution();
+  return;
 }
 
 int main() {
