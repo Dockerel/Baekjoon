efd47a0f2938ddc0ca894811727378796565900e
Giheon Do <dgh0001@naver.com>
2024-03-27 09:21:27 +0900

    divide&conquer

--- ETC/1912/b.cpp
+++ ETC/1912/b.cpp
@@ -3,20 +3,24 @@
   ios_base::sync_with_stdio(0); \
   cin.tie(0);                   \
   cout.tie(0);
+
 using namespace std;
+
 int n, a[100004];
+
 void init() {
   cin >> n;
   for (int i = 0; i < n; i++) cin >> a[i];
   return;
 }
+
 int divideConquer(int start, int end) {
   if (start == end) return a[start];
   int mid = (start + end) / 2;
   int left, right, sum;
   left = right = -2147483640;
   sum = 0;
-  for (int i = mid; i >= 0; i--) {
+  for (int i = mid; i >= start; i--) {
     sum += a[i];
     left = max(left, sum);
   }
@@ -25,9 +29,10 @@ int divideConquer(int start, int end) {
     sum += a[i];
     right = max(right, sum);
   }
-  int side = max(divideConquer(start, mid), divideConquer(mid + 1, end));
-  return max(left + right, side);
+  int subArr = max(divideConquer(start, mid), divideConquer(mid + 1, end));
+  return max(left + right, subArr);
 }
+
 int main() {
   fastIO;
   init();
