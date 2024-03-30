4a31c500acb95c778730c67cbeeb47a58b3480db
Giheon Do <dgh0001@naver.com>
2024-03-31 08:21:38 +0900

    .

--- b.cpp
+++ b.cpp
@@ -1,49 +1,25 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int n;
-int a[8] = {3, 4, 2, 7, 6, 8, 5, 1};
-
-int PATITION(int p, int r) {
-  int x = a[r];
-  int i = p - 1;
-  for (int j = p; j < r; j++) {
-    if (a[j] < x) {
-      i += 1;
-      swap(a[i], a[j]);
-    }
-  }
-  swap(a[i + 1], a[r]);
-  return i + 1;
-}
-
-int RANDOMIZED_PATITION(int p, int r) {
-  random_device rd;
-  mt19937 mt(rd());
-  uniform_int_distribution<int> dist(p, r);
-  int i = dist(mt);
-  swap(a[i], a[r]);
-  return PATITION(p, r);
-}
-
-int SELECT(int p, int r, int k) {
-  if (p == r) return a[p];
-
-  int q = RANDOMIZED_PATITION(p, r);
-  int i = q - p + 1;
-  if (k == i) {
-    return a[q];
-  } else if (k < i) {
-    return SELECT(p, q - 1, k);
-  } else {
-    return SELECT(q + 1, r, k - i);
-  }
-}
+int t, r;
+string s;
 
 int main() {
-  int target = 4;
-  cout << SELECT(0, 7, target) << "\n";
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  cin >> t;
+
+  for (int i = 0; i < t; i++) {
+    cin >> r >> s;
+    for (int j = 0; j < s.length(); j++) {
+      for (int k = 0; k < r; k++) {
+        cout << s[j];
+      }
+    }
+    cout << "\n";
+  }
 
   return 0;
 }
