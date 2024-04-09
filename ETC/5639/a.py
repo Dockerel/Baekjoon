a9bd87c2dd1e37d955c9fcb81bd999aa6ee0637e
Giheon Do <dgh0001@naver.com>
2024-04-10 08:11:34 +0900

    5639

--- ETC/5639/a.py
+++ ETC/5639/a.py
@@ -1,48 +1,27 @@
-# 배열로 다시 해보자
-
 import sys
 
 input = sys.stdin.readline
-
-
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
-
+sys.setrecursionlimit(10**9)
 
 nodes = []
 while 1:
     try:
-        nodes.append(Node(int(input())))
+        nodes.append(int(input()))
     except:
         break
 
-root = nodes[0]
-for i in range(1, len(nodes)):
-    curr = root
-    while 1:
-        if nodes[i].data < curr.data:
-            if curr.left:
-                curr = curr.left
-            else:
-                curr.left = nodes[i]
-                break
-        elif nodes[i].data > curr.data:
-            if curr.right:
-                curr = curr.right
-            else:
-                curr.right = nodes[i]
-                break
 
-
-def postorder(curr):
-    if curr == None:
+def postorder(start, end):
+    if start > end:
         return
-    postorder(curr.left)
-    postorder(curr.right)
-    print(curr.data)
+    mid = end + 1
+    for i in range(start + 1, end + 1):
+        if nodes[start] < nodes[i]:
+            mid = i
+            break
+    postorder(start + 1, mid - 1)
+    postorder(mid, end)
+    print(nodes[start])
 
 
-postorder(root)
+postorder(0, len(nodes) - 1)
