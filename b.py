70adaeae7d8a24e1fa8ebd748babf9d6caff2e1c
Giheon Do <dgh0001@naver.com>
2024-04-23 18:34:18 +0900

    .

--- b.py
+++ b.py
@@ -1,29 +1,16 @@
 import sys
 input=sys.stdin.readline
 
-s2="\"재귀함수가 뭔가요?\""
-s3="\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어."
-s4="마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지."
-s5="그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\""
+def go(a,b):
+    if b%a==0:
+        print("factor")
+    elif a%b==0:
+        print("multiple")
+    else:
+        print("neither")
 
-s6="라고 답변하였지."
-s7="\"재귀함수는 자기 자신을 호출하는 함수라네\""
-tab="____"
-
-n=int(input())
-
-def go(num):
-    if num==n:
-        print(f"{tab*num}{s2}")
-        print(f"{tab*num}{s7}")
-        print(f"{tab*num}{s6}")
-        return
-    print(f"{tab*num}{s2}")
-    print(f"{tab*num}{s3}")
-    print(f"{tab*num}{s4}")
-    print(f"{tab*num}{s5}")
-    go(num+1)
-    print(f"{tab*num}{s6}")
-
-print("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.")
-go(0)
+while 1:
+    n1,n2=list(map(int,input().rstrip().split()))
+    if not n1 and not n2:
+        break
+    go(n1,n2)
