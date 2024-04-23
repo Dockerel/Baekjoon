304c5531b06574f4aacee6a1a51922a98d897a2b
Giheon Do <dgh0001@naver.com>
2024-04-23 18:34:16 +0900

    1655

---
+++ ETC/1655/a.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define fastIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag,
+             tree_order_statistics_node_update>
+    ordered_set;
+int n, num;
+ordered_set X;
+
+void init() {
+  cin >> n;
+  return;
+}
+
+void go() {
+  for (int i = 0; i < n; i++) {
+    cin >> num;
+    X.insert({num, i});
+    cout << X.find_by_order(i / 2)->first << "\n";
+  }
+  return;
+}
+
+int main() {
+  fastIO;
+  init();
+  go();
+  return 0;
+}
