a6ce66f3f3dd0aaa976eb51ad69924d69b44a74c
Giheon Do <dgh0001@naver.com>
2024-05-11 08:06:31 +0900

    16455

--- ETC/16455/a.cpp
+++ ETC/16455/a.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 int kth(vector<int>& a, int k) {
+  random_device rd;
+  shuffle(a.begin(), a.end(), default_random_engine(rd()));
   nth_element(a.begin(), a.begin() + k - 1, a.end());
   return a[k - 1];
 }
