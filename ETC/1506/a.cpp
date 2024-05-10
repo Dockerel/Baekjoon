1b225e7cc3dd3935df3cc270c80d3f3c175664d9
Giheon Do <dgh0001@naver.com>
2024-05-11 08:19:25 +0900

    1506

--- ETC/1506/a.cpp
+++ ETC/1506/a.cpp
@@ -1,17 +1,20 @@
 #include <bits/stdc++.h>
 #define fastIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 #define MAX 104
-#define pii pair<int, int>
-
 using namespace std;
 
-int n, tm;
+typedef pair<int, int> pii;
+
+int n, _tm, min_cost;
 int costs[MAX], visited[MAX];
 string s;
 vector<int> graph[MAX], r_graph[MAX];
 vector<pair<int, pii>> times;
+long long ret;
 
-bool cmp(pii a, pii b) { return a.second > b.second; }
+bool cmp(pair<int, pii> a, pair<int, pii> b) {
+  return a.second.second > b.second.second;
+}
 
 void init() {
   cin >> n;
@@ -23,11 +26,11 @@ void init() {
     for (int j = 0; j < n; j++) {
       if (s[j] == '1') {
         graph[i].push_back(j);
-        graph[j].push_back(i);
+        r_graph[j].push_back(i);
       }
     }
   }
-  tm = 0;
+  _tm = 0;
   return;
 }
 
@@ -35,17 +38,25 @@ void dfs(int here) {
   visited[here] = 1;
   pair<int, pii> temp;
   temp.first = here;
-  temp.second.first = tm++;
+  temp.second.first = _tm++;
   for (auto it : graph[here]) {
     if (!visited[it]) {
       dfs(it);
     }
   }
-  temp.second.second = tm++;
+  temp.second.second = _tm++;
   times.push_back(temp);
 }
 
-void r_dfs(int here) {}
+void r_dfs(int here) {
+  visited[here] = 1;
+  min_cost = min(min_cost, costs[here]);
+  for (auto it : r_graph[here]) {
+    if (!visited[it]) {
+      r_dfs(it);
+    }
+  }
+}
 
 void go() {
   // 정방향 dfs
@@ -61,9 +72,12 @@ void go() {
   memset(visited, 0, sizeof(visited));
   for (auto it : times) {
     if (!visited[it.first]) {
+      min_cost = 1000004;
       r_dfs(it.first);
+      ret += min_cost;
     }
   }
+  cout << ret << "\n";
 }
 
 int main() {
