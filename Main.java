52b1b6e908abc81ad3806def25e36f04fe4e1147
Giheon Do <dgh0001@naver.com>
2024-05-18 08:04:00 +0900

    .

--- Main.java
+++ Main.java
@@ -1,2 +1,18 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class Main {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        bw.write(Integer.toString(n) + "\n1\n");
+        bw.flush();
+        br.close();
+        bw.close();
+    }
 }
--- a.java
+++ a.java
@@ -6,18 +6,21 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class a {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int n1 = Integer.parseInt(st.nextToken());
             int n2 = Integer.parseInt(st.nextToken());
-            bw.write(Integer.toString(n1 + n2) + "\n");
-            bw.flush();
+            sb.append(Integer.toString(n1 + n2)).append("\n");
         }
+        bw.write(sb.toString());
+        bw.flush();
         br.close();
         bw.close();
     }
