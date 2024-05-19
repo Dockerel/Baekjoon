47223c8e0d91c129be5c6617685145007e66abdb
Giheon Do <dgh0001@naver.com>
2024-05-19 19:46:38 +0900

    .

---
+++ ETC/10350/a.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#define fastIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+using namespace std;
+
+int n, ret, _sum;
+int a[10004];
+vector<int> c_sum;
+
+void init() {
+  ret = _sum = 0;
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+    _sum += a[i];
+  }
+}
+
+void go() {
+  c_sum.push_back(0);
+  for (int i = 1; i <= 2 * n; i++) {
+    c_sum.push_back(c_sum[i - 1] + a[(i - 1) % n]);
+  }
+
+  for (int i = 0; i < n; i++) {
+    for (int j = i; j < i + n; j++) {
+      int temp = c_sum[j + 1] - c_sum[i];
+      if (temp < 0) {
+        ret += ceil(double(abs(temp)) / _sum);
+      }
+    }
+  }
+
+  cout << ret << "\n";
+}
+
+int main() {
+  fastIO;
+  init();
+  go();
+  return 0;
+}
+
+// 1 1 1 -2
+// 1 2 3 1
--- b.cpp
+++ b.cpp
@@ -1,41 +1,33 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int main() {
-  int n, m;
-  string pok[100000];
-  string q[100000];
+int n, ss;
+long long ret;
+int a[10004];
+vector<int> cs;
 
-  cin >> n >> m;
+int main() {
+  ret = ss = 0;
+  scanf("%d", &n);
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+    ss += a[i];
+  }
+  cs.push_back(0);
+  for (int i = 1; i <= 2 * n; i++) {
+    cs.push_back(cs[i - 1] + a[(i - 1) % n]);
+  }
 
   for (int i = 0; i < n; i++) {
-    cin >> pok[i];
-  }
-
-  for (int i = 0; i < m; i++) {
-    cin >> q[i];
-  }
-
-  for (int i = 0; i < m; i++) {
-    bool num = true;
-    int index = 0;
-
-    for (int j = 0; j < n; j++) {
-      if (pok[j] == q[i]) {
-        index = j + 1;
-        num = false;
+    for (int j = i; j < i + n; j++) {
+      int temp = cs[j + 1] - cs[i];
+      if (temp < 0) {
+        ret += ceil(double(abs(temp)) / ss);
       }
     }
-
-    if (num == true) {
-      int find = stoi(q[i]);
-      cout << pok[find - 1] << "\n";
-    } else {
-      cout << index << "\n";
-    }
   }
 
-  printf("end");
+  printf("%d\n", ret);
+  return 0;
 }
