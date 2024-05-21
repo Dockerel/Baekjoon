517a70fa178ade2fdd98941af97a81a395940a0a
Giheon Do <dgh0001@naver.com>
2024-05-21 23:41:02 +0900

    .

--- a.java
+++ a.java
@@ -9,12 +9,13 @@ public class a {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     private static StringBuilder sb = new StringBuilder();
+    private static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
 
         int n = Integer.parseInt(br.readLine());
         for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
             int n1 = Integer.parseInt(st.nextToken());
             int n2 = Integer.parseInt(st.nextToken());
             sb.append(Integer.toString(n1 + n2)).append("\n");
