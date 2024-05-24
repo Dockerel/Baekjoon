80e199f151c431a1f128c8cb00072aec91255afb
Giheon Do <dgh0001@naver.com>
2024-05-24 12:02:49 +0900

    .

--- b.py
+++ b.py
@@ -1,16 +1,5 @@
 import sys
-import math
 input=sys.stdin.readline
-n = int(input().strip())
-a = list(map(int, input().strip().split()))
-s = [0]
-for i in range(n*2):
-    s.append(s[-1]+a[i%n])
-print(s)
-exit(0)
-result = 0
-for i in range(n-1):
-    for i2 in range(1, n+1):
-        if s[i2+i]<s[i2-1]:
-            result+=math.ceil(abs(s[i2+i]-s[i2-1])/s[n])
-print(result)
+l1=list(map(int,input().rstrip().split()))
+l2=list(map(int,input().rstrip().split()))
+print(max(l1[0]+l2[1],l1[1]+l2[0]))
