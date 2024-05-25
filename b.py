35556c9e105c46fd0531db064d096dc0bdcff113
Giheon Do <dgh0001@naver.com>
2024-05-25 23:26:07 +0900

    .

--- b.py
+++ b.py
@@ -1,5 +1,7 @@
 import sys
 input=sys.stdin.readline
-l1=list(map(int,input().rstrip().split()))
-l2=list(map(int,input().rstrip().split()))
-print(max(l1[0]+l2[1],l1[1]+l2[0]))
+n,m=list(map(int,input().rstrip().split()))
+for _ in range(n):
+    s=list(input().rstrip())
+    s.reverse()
+    print("".join(s))
