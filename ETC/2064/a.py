49f99a75d3941712d81d467f06b70e05f727910b
Giheon Do <dgh0001@naver.com>
2024-05-26 07:45:22 +0900

    2064

--- ETC/2064/a.py
+++ ETC/2064/a.py
@@ -5,7 +5,7 @@ input = sys.stdin.readline
 n = int(input())
 
 
-def convertAddr(addr):
+def convertAddr(addr) -> int:
     temp = ""
     for num in addr:
         temp += format(num, "08b")
@@ -13,3 +13,36 @@ def convertAddr(addr):
 
 
 addrs = [convertAddr(list(map(int, input().rstrip().split(".")))) for _ in range(n)]
+temp_addr = addrs[0]
+
+
+def isSame() -> bool:
+    for i in range(1, len(addrs)):
+        if addrs[i - 1] != addrs[i]:
+            return False
+    return True
+
+
+cnt = 0
+
+while not isSame():
+    for i in range(len(addrs)):
+        addrs[i] >>= 1
+    cnt += 1
+
+mask = 0xFFFFFFFF
+mask >>= cnt
+mask <<= cnt
+
+
+def convertToIp(num) -> str:
+    temp = []
+    for i in range(4):
+        temp.append(str(num % 256))
+        num >>= 8
+    temp.reverse()
+    return ".".join(temp)
+
+
+print(convertToIp(mask & temp_addr))
+print(convertToIp(mask))
