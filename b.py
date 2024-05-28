f8c3c081e9bb81c7deef4a4f9f91c826ae1af6ed
Giheon Do <dgh0001@naver.com>
2024-05-28 09:47:15 +0900

    .

--- b.py
+++ b.py
@@ -1,7 +1,15 @@
 import sys
 input=sys.stdin.readline
-n,m=list(map(int,input().rstrip().split()))
-for _ in range(n):
-    s=list(input().rstrip())
-    s.reverse()
-    print("".join(s))
+n=int(input())
+_list1=list(map(int,input().rstrip().split()))
+_set=set()
+for num in _list1:
+    _set.add(num)
+_list2=sorted(_set)
+_dict=dict()
+idx=0
+for num in _list2:
+    _dict[num]=idx
+    idx+=1
+for num in _list1:
+    print(_dict[num],end=" ")
