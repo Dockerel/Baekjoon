6afd67fc2f4b60db72239c787c03d7119833bcae
Giheon Do <dgh0001@naver.com>
2024-06-05 22:10:12 +0900

    .

--- b.py
+++ b.py
@@ -1,15 +1,7 @@
-import sys
-input=sys.stdin.readline
-n=int(input())
-_list1=list(map(int,input().rstrip().split()))
-_set=set()
-for num in _list1:
-    _set.add(num)
-_list2=sorted(_set)
-_dict=dict()
-idx=0
-for num in _list2:
-    _dict[num]=idx
-    idx+=1
-for num in _list1:
-    print(_dict[num],end=" ")
+n = int(input())
+
+max_star = 1 + 2 * (n - 1)
+for i in range(n):
+    curr = 1 + 2 * i
+    space = (max_star - curr) // 2
+    print(" " * space + "*" * curr + " " * space)
