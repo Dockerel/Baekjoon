7e29b374f65be9c96b6592235efc4b3118ae7760
Giheon Do <dgh0001@naver.com>
2024-06-09 16:34:38 +0900

    .

--- b.py
+++ b.py
@@ -1,6 +1,5 @@
 n=int(input())
-for i in range(1,n):
-    print(" "*(n-i)+"*"*(i*2-1))
-print("*"*(n*2-1))
-for i in range(n-1,0,-1):
-    print(" "*(n-i)+"*"*(i*2-1))
+for i in range(n,0,-1):
+    print(" "*(n-i)+"*"*(2*i-1))
+for i in range(2,n+1):
+    print(" "*(n-i)+"*"*(2*i-1))
