044f891912e6802b251f1dd19f12ac1e30884e85
Giheon Do <dgh0001@naver.com>
2025-07-30 11:07:22 +0900

    [2025-07-30]: 3933 - (DP, g5) [python]

--- 6. 토 - DP, 비트마스킹/3933/a.py
+++ 6. 토 - DP, 비트마스킹/3933/a.py
@@ -14,10 +14,10 @@ max_num = max(nums)
 dp = [[0 for _ in range(max_num + 1)] for _ in range(5)]
 dp[0][0] = 1
 
-for i in range(1, int(max_num**0.5) + 1):
-    for j in range(1, 4 + 1):
-        for k in range(i**2, max_num + 1):
-            dp[j][k] += dp[j - 1][k - i**2]
+for k in range(1, int(max_num**0.5) + 1):
+    for i in range(1, 4 + 1):
+        for j in range(k**2, max_num + 1):
+            dp[i][j] += dp[i - 1][j - k**2]
 
 for num in nums:
     print(dp[1][num] + dp[2][num] + dp[3][num] + dp[4][num])
