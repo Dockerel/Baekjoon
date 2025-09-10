7d90db1d86d4be7335ca25d8f4c285cc286040f5
Giheon Do <dgh0001@naver.com>
2025-09-10 09:38:51 +0900

    chore: 문제 주제별 재배치

--- template/a.py
+++ template/a.py
@@ -1,6 +1,20 @@
-import sys
+# import sys
 
-input = sys.stdin.readline
+# input = sys.stdin.readline
 
-a = list(map(int, input().split()))
-n, m = list(map(int, input().split()))
+# a = list(map(int, input().split()))
+# n, m = list(map(int, input().split()))
+
+
+n = int(input())
+if n == 0:
+    print("NO")
+    exit(0)
+while n > 0:
+    if n % 3:
+        n -= 1
+    if n % 3:
+        print("NO")
+        exit(0)
+    n //= 3
+print("YES")
