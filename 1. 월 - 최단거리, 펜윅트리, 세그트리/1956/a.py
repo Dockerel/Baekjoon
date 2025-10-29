79af33ba1342c915b58dc778620528349190016d
Giheon Do <dgh0001@naver.com>
2025-10-30 08:45:46 +0900

    [2025-10-28]: 1956 - (최단거리, g4) [python]

--- 1. 월 - 최단거리, 펜윅트리, 세그트리/1956/a.py
+++ 1. 월 - 최단거리, 펜윅트리, 세그트리/1956/a.py
@@ -1,24 +1,24 @@
 import sys
 
-input=sys.stdin.readline
+input = sys.stdin.readline
 
 v, e = map(int, sys.stdin.readline().split())
-graph = [[float('inf')] * v for _ in range(v)]
+graph = [[float("inf")] * v for _ in range(v)]
 for _ in range(e):
     a, b, c = map(int, sys.stdin.readline().split())
-    graph[a-1][b-1] = c
+    graph[a - 1][b - 1] = c
 
 for k in range(v):
     for i in range(v):
         for j in range(v):
-            graph[i][j]=min(graph[i][j],graph[i][k] + graph[k][j])
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
 
-answer = float('inf')
+ret = float("inf")
 for i in range(v):
     for j in range(v):
-        answer = min(answer, graph[i][j] + graph[j][i])
+        ret = min(ret, graph[i][j] + graph[j][i])
 
-if answer == float('inf'):
-    print('-1')
+if ret == float("inf"):
+    print("-1")
 else:
-    print(answer)
+    print(ret)
